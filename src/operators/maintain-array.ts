c51ac944700e412d61183c0d3441125c1396268a
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-02-11 23:55:45 +0100

    fix: only accept meaningful inputs to maintainArray()

--- src/operators/maintain-array.ts
+++ src/operators/maintain-array.ts
@@ -1,11 +1,19 @@
 import { scan } from 'rxjs';
 
 export const maintainArray = <T>() =>
-    scan<[T, boolean], T[]>((array, [value]) => {
+    scan<[T, boolean], T[]>((array, [value, isNewValue]) => {
         const index = array.indexOf(value);
 
         if (index > -1) {
+            if (isNewValue) {
+                throw new Error('The array does already contain the value to be added.');
+            }
+
             return [...array.slice(0, index), ...array.slice(index + 1)];
+        }
+
+        if (!isNewValue) {
+            throw new Error("The array doesn't contain the value to be removed.");
         }
 
         return [...array, value];
--- test/unit/operators/maintain-array.js
+++ test/unit/operators/maintain-array.js
@@ -17,10 +17,30 @@ describe('maintainArray', () => {
     );
 
     it(
+        'should not add a value that is already stored in the array',
+        marbles((helpers) => {
+            const destination = helpers.cold('ab|', { a: [value, true], b: [value, true] }).pipe(maintainArray());
+            const expected = helpers.cold('a#', { a: [value] }, new Error('The array does already contain the value to be added.'));
+
+            helpers.expect(destination).toBeObservable(expected);
+        })
+    );
+
+    it(
         'should remove a new value from the array',
         marbles((helpers) => {
             const destination = helpers.cold('ab|', { a: [value, true], b: [value, false] }).pipe(maintainArray());
             const expected = helpers.cold('ab|', { a: [value], b: [] });
+
+            helpers.expect(destination).toBeObservable(expected);
+        })
+    );
+
+    it(
+        'should not remove a value that is not stored in the array',
+        marbles((helpers) => {
+            const destination = helpers.cold('a|', { a: [value, false] }).pipe(maintainArray());
+            const expected = helpers.cold('#', null, new Error("The array doesn't contain the value to be removed."));
 
             helpers.expect(destination).toBeObservable(expected);
         })
