96a6617cf2644d8aced25bca983dd3e0aa71bbc6
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-05-15 11:26:07 +0200

    fix: use retry() instead of retryWhen()

--- src/operators/retry-backoff.ts
+++ src/operators/retry-backoff.ts
@@ -1,4 +1,4 @@
-import { MonoTypeOperatorFunction, concatMap, defer, iif, retryWhen, throwError, timer } from 'rxjs';
+import { MonoTypeOperatorFunction, defer, iif, retry, throwError, timer } from 'rxjs';
 
 export const retryBackoff =
     <T>(): MonoTypeOperatorFunction<T> =>
@@ -10,18 +10,16 @@ export const retryBackoff =
             let index = 0;
 
             return source.pipe(
-                retryWhen((errors) =>
-                    errors.pipe(
-                        concatMap((error) => {
-                            index += 1;
+                retry({
+                    delay: (error) => {
+                        index += 1;
 
-                            return iif(
-                                () => index < attempts,
-                                timer(interval * index ** 2),
-                                throwError(() => error) // tslint:disable-line:rxjs-throw-error
-                            );
-                        })
-                    )
-                )
+                        return iif(
+                            () => index < attempts,
+                            timer(interval * index ** 2),
+                            throwError(() => error) // tslint:disable-line:rxjs-throw-error
+                        );
+                    }
+                })
             );
         });
