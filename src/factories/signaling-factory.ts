58d843b4cfb2288b724db7b9d6643864d22a2290
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-07-09 22:18:56 +0200

    fix: handle errors thrown when sending something over a WebSocket

--- src/factories/signaling-factory.ts
+++ src/factories/signaling-factory.ts
@@ -1,24 +1,32 @@
-import { Observable, finalize, map, merge, mergeMap, throwError } from 'rxjs';
+import { Observable, Subject, finalize, map, merge, mergeMap, throwError } from 'rxjs';
 import { on } from 'subscribable-things';
 import { TIncomingSignalingEvent, TOutgoingSignalingEvent } from '../types';
 
 export const createSignalingFactory =
     (createWebSocket: (url: string) => WebSocket) =>
     (url: string): readonly [Observable<TIncomingSignalingEvent>, (event: TOutgoingSignalingEvent) => void] => {
+        const errorSubject = new Subject<Error>();
         const webSocket = createWebSocket(url);
         const signalingEvent$ = merge(
             on(webSocket, 'message'),
-            merge(on(webSocket, 'close'), on(webSocket, 'error')).pipe(
-                mergeMap(({ type }) =>
-                    // tslint:disable-next-line:rxjs-throw-error
-                    throwError(() => new Error(`WebSocket fired unexpected event of type "${type}".`))
-                )
-            )
+            merge(
+                merge(...['close', 'error'].map((type) => on(webSocket, type))).pipe(
+                    map(({ type }) => new Error(`WebSocket fired unexpected event of type "${type}".`))
+                ),
+                errorSubject
+                // tslint:disable-next-line:rxjs-throw-error
+            ).pipe(mergeMap((err) => throwError(() => err)))
         ).pipe(
             finalize(() => webSocket.close()),
             map((event) => <TIncomingSignalingEvent>JSON.parse(event.data))
         );
-        const sendSignalingEvent = (event: TOutgoingSignalingEvent) => webSocket.send(JSON.stringify(event));
+        const sendSignalingEvent = (event: TOutgoingSignalingEvent) => {
+            try {
+                webSocket.send(JSON.stringify(event));
+            } catch (err) {
+                errorSubject.next(err);
+            }
+        };
 
         return [signalingEvent$, sendSignalingEvent] as const;
     };
